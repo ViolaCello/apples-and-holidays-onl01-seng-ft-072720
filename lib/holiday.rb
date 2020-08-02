0b92064f79fa5b64518c250e4993f80abde823d4
Anthony Lepore <violacellonyc@gmail.com>
2020-08-02 15:28:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 
 holiday_hash.each do |key, value|
     if key == season
-      holiday_hash[]
+      holiday_hash[season]
     end
   end
 
