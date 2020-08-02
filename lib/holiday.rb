a1246a334a030fc581d4a9a1983171dfe296c06b
Anthony Lepore <violacellonyc@gmail.com>
2020-08-02 15:16:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,12 +18,27 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  
+    holiday_supplies[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
+  
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+
+holiday_hash.each do |key, value|
+ 
+  if key == :winter
+    
+    value.each do |holiday, stuff|
+      
+      stuff.push(supply)
+     
+    end
+  end
+end
 
 end
 
@@ -31,6 +46,16 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+
+holiday_hash.each do |key, value|
+  if key == :spring
+    value.each do |holiday, stuff|
+      if holiday == :memorial_day
+    stuff.push(supply)
+  end
+end
+  end
+end
 
 end
 
